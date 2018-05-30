34cdbe27391bf275a8b5097bb41fba9b4279db34
Lubomyr Grynyshyn <lgryn@softserveinc.com>
2018-05-30 11:23:43 +0300

    Add assets

--- client/scenes/main.js
+++ client/scenes/main.js
@@ -1,5 +1,10 @@
 export class Main extends Phaser.Scene {
+  preload() {
+    this.load.image('contra', 'assets/contra.png');
+  }
+
   create() {
-    this.add.text(100, 100, 'Hello world!', { fill: '#0f0' });
+    this.add.text(340, 100, 'Hello world!', { fill: '#0f0' });
+    this.add.image(200, 200, 'contra');
   }
 }
